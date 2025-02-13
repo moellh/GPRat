16983e243663f0f41a60af4166b507324d974d91
Henrik Möllmann <henmoell@gmail.com>
2025-02-13 15:44:18 +0100

    Simplyfied run_cpp files

--- examples/gpxpy_cpp/run_cpp.sh
+++ examples/gpxpy_cpp/run_cpp.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 ################################################################################
-# Run C++ test code (see test_cpp/)
+# Run C++ test code
 ################################################################################
 
-# Exit on error (non-zero status); Print each command before executing it
+# Exit on error (non-zero status).
+# Print each command before executing it.
 set -ex
 
-# Configuration ------------------------------------------------------------ {{{
+# Configure {{{
 
 # Load spack environment
 source $HOME/spack/share/spack/setup-env.sh
@@ -16,49 +17,49 @@ spack env activate gpxpy
 # Set cmake command
 export CMAKE_COMMAND=$(which cmake)
 
-# Activate APEX output to stdout
-export APEX_SCREEN_OUTPUT=1
-
 # Configure MKL
-export MKL_CONFIG='-DMKL_ARCH=intel64 -DMKL_LINK=dynamic \
-                   -DMKL_INTERFACE_FULL=intel_lp64 -DMKL_THREADING=sequential'
+export MKL_CONFIG='-DMKL_ARCH=intel64 -DMKL_LINK=dynamic -DMKL_INTERFACE_FULL=intel_lp64 -DMKL_THREADING=sequential'
 
 # Get CUDA architecture
-export CUDA_ARCH=$(nvidia-smi --query-gpu=compute_cap --format=csv,noheader | \
-                   awk -F '.' '{print $1$2}')
+export CUDA_ARCH=$(nvidia-smi --query-gpu=compute_cap --format=csv,noheader | awk -F '.' '{print $1$2}')
 
-# }}} ----------------------------------------------------- end of Configuration
+export CUDA_FLAGS="
+    --cuda-gpu-arch=sm_${CUDA_ARCH}
+"
 
-# Compilation -------------------------------------------------------------- {{{
+export CMAKE_OPTIONS="
+    -DCMAKE_BUILD_TYPE=Release
+    -DHPX_WITH_DYNAMIC_HPX_MAIN=ON
+    -DCMAKE_C_COMPILER=$(which clang)
+    -DCMAKE_CXX_COMPILER=$(which clang++)
+    -DCMAKE_CUDA_COMPILER=$(which clang++)
+    -DCMAKE_CUDA_FLAGS=--cuda-path=${CUDA_HOME}
+    -DCMAKE_CUDA_ARCHITECTURES=${CUDA_ARCH}
+    ${MKL_CONFIG}
+"
+
+# }}}
+
+# Compile {{{
 
 # Reset build directory
 rm -rf build && mkdir build && cd build
 
-# copy apex.conf if exists
+# Copy apex.conf if exists
 if [ -f ../apex.conf ]; then
     cp ../apex.conf .
 fi
 
 # Configure project
-$CMAKE_COMMAND .. \
-    -DCMAKE_BUILD_TYPE=Debug \
-    -DHPX_WITH_DYNAMIC_HPX_MAIN=ON \
-    -DCMAKE_C_COMPILER=$(which clang) \
-    -DCMAKE_CXX_COMPILER=$(which clang++) \
-    -DCMAKE_CUDA_COMPILER=$(which clang++) \
-    -DCMAKE_CUDA_FLAGS="--cuda-gpu-arch=sm_${CUDA_ARCH} \
-                        --cuda-path=${CUDA_HOME}" \
-    -DCMAKE_CUDA_ARCHITECTURES=${CUDA_ARCH} \
-    ${MKL_CONFIG} \
-    "$@"
+$CMAKE_COMMAND .. $CMAKE_OPTIONS "$@"
 
 # Build project
 make -j VERBOSE=1 all
 
-# }}} ------------------------------------------------------- end of Compilation
+# }}}
 
-# Running test code -------------------------------------------------------- {{{
+# Run {{{
 
 ./gpxpy_cpp
 
-# }}} ------------------------------------------------- end of Running test code
+# }}}
--- examples/gpxpy_cpp/run_cpp_simcl1.sh
+++ examples/gpxpy_cpp/run_cpp_simcl1.sh
@@ -1,63 +1,15 @@
 #!/bin/bash
 
 ################################################################################
-# Run C++ test code (see test_cpp/)
+# Run C++ test code on simcl1n1 or simcl1n2
 ################################################################################
 
-# Exit on error (non-zero status); Print each command before executing it
+# Exit on error (non-zero status).
+# Print each command before executing it.
 set -ex
-
-# Configuration ------------------------------------------------------------ {{{
 
 # Load modules
 module load clang/17.0.1
 module load cuda/12.2.2
 
-# Load spack environment
-source $HOME/spack/share/spack/setup-env.sh
-spack env activate gpxpy
-
-# Set cmake command
-export CMAKE_COMMAND=$(which cmake)
-
-# Configure APEX
-export APEX_SCREEN_OUTPUT=1
-
-# Configure MKL
-export MKL_CONFIG='-DMKL_ARCH=intel64 -DMKL_LINK=dynamic \
-                   -DMKL_INTERFACE_FULL=intel_lp64 -DMKL_THREADING=sequential'
-
-# Get CUDA architecture
-export CUDA_ARCH=$(nvidia-smi --query-gpu=compute_cap --format=csv,noheader | \
-                   awk -F '.' '{print $1$2}')
-
-# }}} ----------------------------------------------------- end of Configuration
-
-# Compilation ------------------------------------------------------------------
-
-# Goto project directory
-cd test_cpp
-
-# Reset build directory
-rm -rf build && mkdir build && cd build
-
-# Configure the project
-$CMAKE_COMMAND .. \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DHPX_WITH_DYNAMIC_HPX_MAIN=ON \
-    -DCMAKE_C_COMPILER=$(which clang) \
-    -DCMAKE_CXX_COMPILER=$(which clang++) \
-    -DCMAKE_CUDA_COMPILER=$(which clang++) \
-    -DCMAKE_CUDA_FLAGS="--cuda-gpu-arch=sm_80 \
-                        --cuda-path=/usr/local.nfs/sw/cuda/cuda-12.2.2" \
-    -DCMAKE_CUDA_ARCHITECTURES=80 \
-    ${MKL_CONFIG}
-
-# Build the project
-make -j VERBOSE=1 all
-
-# Running test code ------------------------------------------------------------
-
-../test_cpp
-
-# ----------------------------------------------------- end of Running test code
+./run_cpp.sh
