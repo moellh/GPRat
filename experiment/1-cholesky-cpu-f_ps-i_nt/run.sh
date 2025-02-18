c6ecfcbf36ef533288cc5b6ba54b066f838cfc5a
Henrik Möllmann <henmoell@gmail.com>
2025-02-18 13:02:56 +0100

    Testing n_cores

--- experiment/1-cholesky-cpu-f_ps-i_nt/run.sh
+++ experiment/1-cholesky-cpu-f_ps-i_nt/run.sh
@@ -3,11 +3,11 @@
 source $HOME/spack/share/spack/setup-env.sh
 spack env activate gpxpy
 
-N_CORES=(24) # (1 2 3 4 6 8 12 16 24 32 48)
-N_TRAIN=(4096 8192 16384 32768) # 32768, 65536
-N_TILES=(8 16 32 64)
+N_CORES=(8 12 16 24 32 48) # (1 2 3 4 6 8 12 16 24 32 48)
+N_TRAIN=(32768) # 32768, 65536
+N_TILES=(64)
 N_REG=(8)
-N_LOOPS=(10)
+N_LOOPS=(5)
 
 for core in "${N_CORES[@]}"; do
   for train in "${N_TRAIN[@]}"; do
