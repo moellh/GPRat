8ef2a8605d5437e5936c9f9d44b09974b6a84d78
Henrik Möllmann <henmoell@gmail.com>
2025-02-23 16:52:16 +0100

    Finalize test 1

--- experiment/1-2-cholesky-cpu/run.sh
+++ experiment/1-2-cholesky-cpu/run.sh
@@ -3,6 +3,10 @@
 source $HOME/spack/share/spack/setup-env.sh
 spack env activate gpxpy
 
+N_CORES=(6 12 24 48)
+N_REG=(8)
+N_LOOPS=(11)
+
 run_experiment() {
   for core in "${N_CORES[@]}"; do
     for train in "${N_TRAIN[@]}"; do
@@ -17,67 +21,26 @@ run_experiment() {
   done
 }
 
-# N_CORES=(6 12 24 48)
-# N_TRAIN=(1024)
-# N_TILES=(1 2 4 8 16 32 64)
-# N_REG=(8)
-# N_LOOPS=(10)
-#
-# run_experiment
-#
-# N_CORES=(6 12 24 48)
-# N_TRAIN=(2048)
-# N_TILES=(1 2 4 8 16 32 64 128)
-# N_REG=(8)
-# N_LOOPS=(10)
-#
-# run_experiment
-#
-# N_CORES=(6 12 24 48)
-# N_TRAIN=(4096)
-# N_TILES=(2 4 8 16 32 64 128)
-# N_REG=(8)
-# N_LOOPS=(10)
-#
-# run_experiment
-#
-# N_CORES=(6 12 24 48)
-# N_TRAIN=(8192)
-# N_TILES=(4 8 16 32 64 128 256)
-# N_REG=(8)
-# N_LOOPS=(10)
-#
-# run_experiment
-
-N_CORES=(48)
-N_TRAIN=(8192)
-N_TILES=(128 256)
-N_REG=(8)
-N_LOOPS=(10)
-
+N_TRAIN=(1024)
+N_TILES=(1 2 4 8 16 32 64)
 run_experiment
 
-N_CORES=(6 12 24 48)
+N_TRAIN=(2048)
+N_TILES=(2 4 8 16 32 64 128)
+run_experiment
+
+N_TRAIN=(4096)
+N_TILES=(2 4 8 16 32 64 128)
+run_experiment
+
+N_TRAIN=(8192)
+N_TILES=(4 8 16 32 64 128 256)
+run_experiment
+
 N_TRAIN=(16384)
 N_TILES=(8 16 32 64 128 256)
-N_REG=(8)
-N_LOOPS=(10)
-
 run_experiment
 
-N_CORES=(6 12 24 48)
 N_TRAIN=(32768)
-N_TILES=(16 32 64 128 256 512)
-N_REG=(8)
-N_LOOPS=(10)
-
-run_experiment
-
-# cpu part of test 5 uses results from test 1 and this (n_train=65536, optimal n_cores)
-N_CORES=(48)
-N_TRAIN=(65536)
-N_TILES=(16 32 64 128 256 512)
-N_REG=(8)
-N_LOOPS=(10)
-
+N_TILES=(16 32 64 128 256)
 run_experiment
---
+++ experiment/slurm_job-1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#SBATCH -w simcl1n1
+#SBATCH --job-name="gp_1"
+#SBATCH --output=job_gprat-1.out #SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --exclusive
+
+gprat_dir="/data/scratch/mllmanhk/GPRat"
+msd_dir="$gprat_dir/data/generators/msd_simulator"
+export PYTHONPATH=$PYTHONPATH:${gprat_dir}/examples/gpxpy_python/install_python/
+
+# Download GPRat repo
+mkdir -p $gprat_dir
+rm -rf $gprat_dir
+git clone https://github.com/moellh/GPRat.git $gprat_dir
+cd $gprat_dir
+git switch experiment
+
+# Generate data
+cd $msd_dir
+./run_msd.sh
+
+# Test 1
+echo "=== Starting Test 1"
+cd $gprat_dir
+./compile_gpxpy_python_simcl1.sh -DGPXPY_WITH_CUDA=ON
+cd experiment/1-2-cholesky-cpu/
+mkdir -p apex
+./run_simcl1.sh
+timestamp=$(date +"%m-%d_%H-%M-%S")
+results_dir=$HOME/results/1/${timestamp}
+mkdir -p ${results_dir}
+cp output.csv ${results_dir}/output.csv
+cp -r apex/ ${results_dir}/apex/
+echo "=== Finished Test 1"
