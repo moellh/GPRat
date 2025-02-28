faefe09b38094c02efc9d82ceeb3b464d5a8542a
Henrik Möllmann <henmoell@gmail.com>
2025-02-28 09:40:00 +0100

    Changed test 1 to 512 only

--- experiment/test-1-cholesky-cpu/run.sh
+++ experiment/test-1-cholesky-cpu/run.sh
@@ -3,10 +3,6 @@
 source $HOME/spack/share/spack/setup-env.sh
 spack env activate gpxpy
 
-N_CORES=(6 12 24 48)
-N_REG=(8)
-N_LOOPS=(11)
-
 run_experiment() {
   for core in "${N_CORES[@]}"; do
     for train in "${N_TRAIN[@]}"; do
@@ -21,31 +17,9 @@ run_experiment() {
   done
 }
 
-N_TRAIN=(1024)
-N_TILES=(1 2 4 8 16 32 64)
-run_experiment
-
-N_TRAIN=(2048)
-N_TILES=(2 4 8 16 32 64 128)
-run_experiment
-
-N_TRAIN=(4096)
-N_TILES=(2 4 8 16 32 64 128)
-run_experiment
-
-N_TRAIN=(8192)
-N_TILES=(4 8 16 32 64 128 256)
-run_experiment
-
-N_TRAIN=(16384)
-N_TILES=(8 16 32 64 128 256)
-run_experiment
-
+N_CORES=(6 12 24 48)
 N_TRAIN=(32768)
-N_TILES=(16 32 64 128 256)
-run_experiment
-
-N_CORES=(48)
-N_TRAIN=(65536)
-N_TILES=(16 32 64 128 256)
+N_TILES=(512)
+N_REG=(8)
+N_LOOPS=(11)
 run_experiment
