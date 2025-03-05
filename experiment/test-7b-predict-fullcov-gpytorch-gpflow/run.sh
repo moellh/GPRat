ad4d0dbd6623a0fb130bcb6f0aa7e7dfb6b3532b
Henrik Möllmann <henmoell@gmail.com>
2025-03-05 11:40:20 +0100

    Fixed iterations for test 7,8

--- experiment/test-7b-predict-fullcov-gpytorch-gpflow/run.sh
+++ experiment/test-7b-predict-fullcov-gpytorch-gpflow/run.sh
@@ -6,49 +6,123 @@ spack env activate gpxpy
 # Test 7b
 
 # GPyTorch
-N_CORES=(48)
-N_TRAIN=(128 256 512 1024 2048 4096 8192 16384 32768)
-N_TEST=(128 256 512 1024 2048 4096 8192 16384 32768)
-N_REG=(8)
-N_LOOPS=(10)
 
 cd gpytorch
 
-for core in "${N_CORES[@]}"; do
-  for train in "${N_TRAIN[@]}"; do
-    for test in "${N_TEST[@]}"; do
-      for reg in "${N_REG[@]}"; do
-        for loop in "${N_LOOPS[@]}"; do
-          python3 run_gpytorch_cpu.sh --n_cores $core --n_train $train --n_test $test --n_reg $reg --n_loops $loop
-          python3 run_gpytorch_gpu.sh --n_cores $core --n_train $train --n_test $test --n_reg $reg --n_loops $loop
+run_experiment_gpytorch() {
+  for core in "${N_CORES[@]}"; do
+    for train in "${N_TRAIN[@]}"; do
+      for test in "${N_TEST[@]}"; do
+        for reg in "${N_REG[@]}"; do
+          for loop in "${N_LOOPS[@]}"; do
+            python3 run_gpytorch_cpu.sh --n_cores $core --n_train $train --n_test $test --n_reg $reg --n_loops $loop
+            python3 run_gpytorch_gpu.sh --n_cores $core --n_train $train --n_test $test --n_reg $reg --n_loops $loop
+          done
         done
       done
     done
   done
-done
+}
+
+N_CORES=(48)
+N_REG=(8)
+N_LOOPS=(10)
+
+N_TRAIN=(128)
+N_TEST=(128)
+run_experiment_gpytorch
+
+N_TRAIN=(256)
+N_TEST=(256)
+run_experiment_gpytorch
+
+N_TRAIN=(512)
+N_TEST=(512)
+run_experiment_gpytorch
+
+N_TRAIN=(1024)
+N_TEST=(1024)
+run_experiment_gpytorch
+
+N_TRAIN=(2048)
+N_TEST=(2048)
+run_experiment_gpytorch
+
+N_TRAIN=(4096)
+N_TEST=(4096)
+run_experiment_gpytorch
+
+N_TRAIN=(8192)
+N_TEST=(8192)
+run_experiment_gpytorch
+
+N_TRAIN=(16384)
+N_TEST=(16384)
+run_experiment_gpytorch
+
+N_TRAIN=(32768)
+N_TEST=(32768)
+run_experiment_gpytorch
 
 cd ..
 
 # GPflow
-N_CORES=(48)
-N_TRAIN=(128 256 512 1024 2048 4096 8192 16384 32768)
-N_TEST=(128 256 512 1024 2048 4096 8192 16384 32768)
-N_REG=(8)
-N_LOOPS=(10)
 
 cd gpflow
 
-for core in "${N_CORES[@]}"; do
-  for train in "${N_TRAIN[@]}"; do
-    for test in "${N_TEST[@]}"; do
-      for reg in "${N_REG[@]}"; do
-        for loop in "${N_LOOPS[@]}"; do
-          python3 run_gpflow_cpu.sh --n_cores $core --n_train $train --n_test $test --n_reg $reg --n_loops $loop
-          python3 run_gpflow_gpu.sh --n_cores $core --n_train $train --n_test $test --n_reg $reg --n_loops $loop
+run_experiment_gpflow() {
+  for core in "${N_CORES[@]}"; do
+    for train in "${N_TRAIN[@]}"; do
+      for test in "${N_TEST[@]}"; do
+        for reg in "${N_REG[@]}"; do
+          for loop in "${N_LOOPS[@]}"; do
+            python3 run_gpflow_cpu.sh --n_cores $core --n_train $train --n_test $test --n_reg $reg --n_loops $loop
+            python3 run_gpflow_gpu.sh --n_cores $core --n_train $train --n_test $test --n_reg $reg --n_loops $loop
+          done
         done
       done
     done
   done
-done
+}
+
+N_CORES=(48)
+N_REG=(8)
+N_LOOPS=(10)
+
+N_TRAIN=(128)
+N_TEST=(128)
+run_experiment_gpflow
+
+N_TRAIN=(256)
+N_TEST=(256)
+run_experiment_gpflow
+
+N_TRAIN=(512)
+N_TEST=(512)
+run_experiment_gpflow
+
+N_TRAIN=(1024)
+N_TEST=(1024)
+run_experiment_gpflow
+
+N_TRAIN=(2048)
+N_TEST=(2048)
+run_experiment_gpflow
+
+N_TRAIN=(4096)
+N_TEST=(4096)
+run_experiment_gpflow
+
+N_TRAIN=(8192)
+N_TEST=(8192)
+run_experiment_gpflow
+
+N_TRAIN=(16384)
+N_TEST=(16384)
+run_experiment_gpflow
+
+N_TRAIN=(32768)
+N_TEST=(32768)
+run_experiment_gpflow
 
 cd ..
