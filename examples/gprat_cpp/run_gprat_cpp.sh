fd1e808368036e13c7d43bfe857a5a9631012bbb
Henrik Möllmann <henmoell@gmail.com>
2025-04-14 20:38:49 +0200

    feat(example): add optional GPU support to C++ example
    Using "gpu --use_gpu" as an argument in ./examples/gprat_cpp/ now
    performs the example on the GPU. Otherwise and if GPRat was compiled
    without CUDA, it executes the CPU example as done previously. Using "gpu
    cpu" uses the GPU Spack environment and uses the CPU for computations.
    No additional argument or just "cpu" also performs computations on the
    CPU using the CPU Spack environment. That is, it also compiles the
    example script using the given Spack environment.
    
    Additionaly, the output.csv file generated with the test, has an
    additional Target column which contains "cpu" or "gpu" depending on the
    Target used for computations. When performing the example on the GPU,
    the GP optimize function is not executed and puts -1 into the respective
    column.

--- examples/gprat_cpp/run_gprat_cpp.sh
+++ examples/gprat_cpp/run_gprat_cpp.sh
@@ -6,12 +6,46 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 ################################################################################
 # Configurations
 ################################################################################
-# Load GCC compiler
-spack load gcc@14.2.0
-spack load cmake
 
-# Activate environment
-spack env activate gprat_cpu_gcc
+if [[ -z "$1" ]]; then
+    echo "First parameter is missing. Using default: cpu"
+    cpu=1
+    gpu=0
+elif [[ "$1" == "cpu" ]]; then
+    cpu=1
+    gpu=0
+elif [[ "$1" == "gpu" ]]; then
+    cpu=0
+    gpu=1
+else
+    echo "Please specify input parameter: cpu/gpu"
+    exit 1
+fi
+
+if [[ $cpu -eq 1 ]]; then
+    # Load GCC compiler
+    module load gcc/14.1.0
+
+    # Activate spack environment
+    spack env activate gprat_cpu_gcc
+
+elif [[ $gpu -eq 1 ]]; then
+    # Load Clang compiler and CUDA library
+    module load clang/17.0.1
+    module load cuda/12.0.1
+
+    # Activate spack environment
+    spack env activate gprat_gpu_clang
+
+    if [[ -z "$2" ]]; then
+        echo "Second parameter is missing. Using default: Run computations on CPU"
+    elif [[ "$2" == "gpu" ]]; then
+        use_gpu="--use_gpu"
+    elif [[ "$2" != "cpu" ]]; then
+        echo "Please specify second input parameter: cpu/gpu"
+        exit 1
+    fi
+fi
 
 # Configure APEX
 export APEX_SCREEN_OUTPUT=0
@@ -21,12 +55,15 @@ export APEX_DISABLE=1
 # Compile code
 ################################################################################
 rm -rf build && mkdir build && cd build
+
 # Configure the project
-cmake .. -DCMAKE_BUILD_TYPE=Release -DGPRat_DIR=./lib/cmake/GPRat
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+         -DGPRat_DIR=./lib/cmake/GPRat
+
  # Build the project
 make -j
 
 ################################################################################
 # Run code
 ################################################################################
-./gprat_cpp
+./gprat_cpp $use_gpu
