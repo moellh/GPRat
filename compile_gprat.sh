ebcd898aa78ac2b10ce533f9a197f9519f4c8275
Henrik Möllmann <henmoell@gmail.com>
2025-04-14 13:58:04 +0200

    build: add cpu/gpu option to compile_gprat.sh
    - Compiles GPRat using the CPU implementation with gprat_cpu_gcc when
      using no or "cpu" as the second argument.
    - Compiles GPRat using the GPU implementation with gprat_gpu_clang when
      using "gpu" as the second argument.
    - Throws error if neither "", "cpu", nor "gpu" is the second argument.
    - Uses Clang for compiling GPU implementation

--- compile_gprat.sh
+++ compile_gprat.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # $1: python/cpp
+# $2: cpu/gpu
 ################################################################################
 set -e  # Exit immediately if a command exits with a non-zero status.
-#set -x  # Print each command before executing it.
 
 ################################################################################
 # Configurations
 ################################################################################
-# Load GCC compiler
-module load gcc/14.2.0
 
-# Activate spack environment
-spack env activate gprat_cpu_gcc
+# Release:	release-linux
+# Debug:	dev-linux
+export PRESET=release-linux
 
 # Bindings
 if [[ "$1" == "python" ]]
@@ -23,22 +22,66 @@ then
 	export BINDINGS=OFF
 	export INSTALL_DIR=$(pwd)/examples/gprat_cpp
 else
-    echo "Please specify input parameter: python/cpp"
+    echo "Please specify first input parameter: python/cpp"
     exit 1
 fi
 
-# Release:	release-linux
-# Debug:	dev-linux
-export PRESET=release-linux
+if [[ -z "$2" ]]; then
+    echo "Second parameter is missing. Using default: cpu"
+    cpu=1
+    gpu=0
+elif [[ "$2" == "cpu" ]]; then
+    cpu=1
+    gpu=0
+elif [[ "$2" == "gpu" ]]; then
+    cpu=0
+    gpu=1
+else
+    echo "Please specify input parameter: cpu/gpu"
+    exit 1
+fi
+
+if [[ $cpu -eq 1 ]]; then
+    # Load GCC compiler
+    module load gcc/14.1.0
+
+    # Activate spack environment
+    spack env activate gprat_cpu_gcc
+
+    cmake --preset $PRESET \
+        -DGPRAT_BUILD_BINDINGS=$BINDINGS \
+        -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+        -DHPX_IGNORE_BOOST_COMPATIBILITY=ON \
+        -DGPRAT_ENABLE_FORMAT_TARGETS=OFF \
+        -DGPRAT_WITH_CUDA=OFF
+
+elif [[ $gpu -eq 1 ]]; then
+    # Load Clang compiler and CUDA library
+    module load clang/17.0.1
+    module load cuda/12.0.1
+
+    # Activate spack environment
+    spack env activate gprat_gpu_clang
+
+    CUDA_ARCH=$(nvidia-smi --query-gpu=compute_cap --format=csv,noheader | awk -F '.' '{print $1$2}')
+
+    cmake --preset $PRESET \
+        -DGPRAT_BUILD_BINDINGS=$BINDINGS \
+        -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+        -DHPX_IGNORE_BOOST_COMPATIBILITY=ON \
+        -DGPRAT_ENABLE_FORMAT_TARGETS=OFF \
+        -DCMAKE_C_COMPILER=$(which clang) \
+        -DCMAKE_CXX_COMPILER=$(which clang++) \
+        -DCMAKE_CUDA_COMPILER=$(which clang++) \
+        -DCMAKE_CUDA_FLAGS=--cuda-path=${CUDA_HOME} \
+        -DCMAKE_CUDA_ARCHITECTURES=${CUDA_ARCH} \
+        -DGPRAT_WITH_CUDA=ON
+fi
 
 ################################################################################
 # Compile code
 ################################################################################
-cmake --preset $PRESET \
-      -DGPRAT_BUILD_BINDINGS=$BINDINGS \
-      -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-      -DHPX_IGNORE_BOOST_COMPATIBILITY=ON \
-      -DGPRAT_ENABLE_FORMAT_TARGETS=OFF
+
 cmake --build --preset $PRESET -- -j
 cmake --install build/$PRESET
 
